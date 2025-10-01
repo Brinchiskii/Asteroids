5671fec6c5f6d89808c8efe28082dd39f87ff33a
Sebastian Brinch <sebastian@tbrinch.com>
2025-10-01 20:58:01 +0200

    circleshape class for representing circle objects

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
     
     print("Starting Asteroids!")
